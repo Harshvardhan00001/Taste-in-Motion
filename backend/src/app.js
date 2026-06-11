dcce61ea2dc79603a39897e64da84511a96f4cdb
Harshvardhan <harshvardhan2672@gmail.com>
2026-06-11 15:30:20 +0530

    fix frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 
 const app=express();
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://taste-in-motion.vercel.app",
     credentials: true
 }));
 app.use(cookieParser())
