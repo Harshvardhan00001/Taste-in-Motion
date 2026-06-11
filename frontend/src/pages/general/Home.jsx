6301e2b8ca72fa5cdac4f7aa83c02d7607c2809d
Harshvardhan <harshvardhan2672@gmail.com>
2026-06-11 13:51:21 +0530

    fix cors and api url

--- frontend/src/pages/general/Home.jsx
+++ frontend/src/pages/general/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     // Autoplay behavior is handled inside ReelFeed
 
     useEffect(() => {
-        axios.get("http://localhost:3000/api/food", { withCredentials: true })
+        axios.get("https://taste-in-motion-vycv.vercel.app/api/food", { withCredentials: true })
             .then(response => {
 
                 console.log(response.data);
@@ -25,7 +25,7 @@ const Home = () => {
         const token = localStorage.getItem("token"); // or sessionStorage, depending on your setup
 
         const response = await axios.post(
-            "http://localhost:3000/api/food/like",
+            "https://taste-in-motion-vycv.vercel.app/api/food/like",
             { foodId: item._id },
             {
                 withCredentials: true,
@@ -57,7 +57,7 @@ const Home = () => {
 
 
     async function saveVideo(item) {
-        const response = await axios.post("http://localhost:3000/api/food/save", { foodId: item._id }, { withCredentials: true })
+        const response = await axios.post("https://taste-in-motion-vycv.vercel.app/api/food/save", { foodId: item._id }, { withCredentials: true })
         
         if(response.data.save){
             setVideos((prev) => prev.map((v) => v._id === item._id ? { ...v, savesCount: v.savesCount + 1 } : v))
