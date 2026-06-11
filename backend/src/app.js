afb874a38efc3a90fe35d721370d4b5b3de99f5d
Harshvardhan <harshvardhan2672@gmail.com>
2026-06-11 14:23:31 +0530

    fix cors and api url backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 
 const app=express();
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://taste-in-motion-q6dv.vercel.app",
     credentials: true
 }));
 app.use(cookieParser())
